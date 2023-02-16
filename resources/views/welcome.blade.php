beee5f17d82b1d0e8c6034a30ccdd23114956b12
AdrianBottini <adrianboh@gmail.com>
2023-02-16 16:37:07 +0100

    ciao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ciao a tutti
                 </div>
 
                 <div class="links">

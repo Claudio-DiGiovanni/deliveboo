d09cc187921ad372e8646e00850afff48d59ae80
AdrianBottini <adrianboh@gmail.com>
2023-02-17 17:13:47 +0100

    font.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -3,16 +3,22 @@ require('./bootstrap');
 import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
+import AdminHome from './pages/AdminHome';
 Vue.use(VueRouter);
 
 const routes = [
     {
         path:'/',
         name:'restaurants',
+        component:AdminHome,
+    }
+]
+
 const router = new VueRouter({
     mode: 'history',
     routes,
 });
+
 new Vue({
     el: '#root',
     render: h => h(App),

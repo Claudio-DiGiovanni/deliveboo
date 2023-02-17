ac0bed69eacab7cbf3d09658b0ae11f816085546
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-02-17 18:21:46 +0100

    correct

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,12 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
-<<<<<<< HEAD:resources/views/guest/home.blade.php
     <script src="{{ asset('js/front.js') }}" defer></script>
-=======
-    {{-- importare il javascript --}}
-    <script src="{{ asset('../../js/front.js') }}" defer></script>
->>>>>>> b257ecff47b0b60135b3a70c3290c473cb415d7c:resources/views/admin/home.blade.php
 </head>
 <body>
     <div id="root"></div>

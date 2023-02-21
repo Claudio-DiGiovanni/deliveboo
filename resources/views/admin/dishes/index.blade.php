44b3e395f5fe6e6bb8f731997a8606ee2a788c8a
DiceglieA <angelodiceglie10@gmail.com>
2023-02-21 09:09:43 +0100

    index + bottoni

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -4,12 +4,20 @@
     <h1>Lista piatti:</h1>
     <ol>
         @foreach ($dishes as $dish)
-            <li> <a href="{{route('admin.dishes.show',['dish'=>$dish])}}">{{$dish->name}}</a>
-                <ul>
-                    <li>{{$dish->price/100}} €</li>
-                    {{-- <li><img src="{{$dish->image}}" alt=""></li> --}}
-                </ul>
-            </li>
+            <div class="row">
+                <div class="col-5">
+                    <span href="{{route('admin.dishes.show',['dish'=>$dish])}}">{{$dish->name}}</span>
+                    <span>{{$dish->price/100}} €</span>
+                </div>
+                <div class="col-3">
+                    <img class="w-25" src="{{$dish->image}}" alt="">
+                </div>
+                <div class="col-4">
+                    <a href="{{ route('admin.dishes.show', ['dish' => $dish]) }}" class="btn btn-outline-info">Info</a>
+                    {{-- <a href="{{ route('admin.dishes.edit', ['dish' => $dish]) }}" class="btn btn-outline-success">Modifica</a> --}}
+                    {{-- <a class="btn btn-outline-danger">Elimina</a> --}}
+                </div>
+            </div>
         @endforeach
     </ol>
 
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            {{-- <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
@@ -16,7 +16,8 @@
 
                     {{ __('You are logged in!') }}
                 </div>
-            </div>
+            </div> --}}
+            <h1>Ciao capo, benvenuto nel tuo ristorante</h1>
         </div>
     </div>
 </div>

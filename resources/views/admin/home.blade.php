55d96dfd90bd852b68869c7ca07cbbc1ec7bee52
VincenzoZ24 <zitov287@gmail.com>
2023-02-22 17:35:37 +0100

    modifiche style

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -22,8 +22,8 @@
            <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                    <div class="card">
-                      <div class="card-body">
+                    <div class="card h-100">
+                      <div class="card-body d-flex flex-column justify-content-between">
                         <h5 class="card-title text-center">Menù piatti</h5>
                         <img src="https://media-assets.lacucinaitaliana.it/photos/638e66dca4734137a7a3e238/16:9/w_1280,c_limit/spaghetti-spezzati-con-sugo-di-pesce1.jpg" class="card-img-top" alt="...">
 
@@ -34,8 +34,8 @@
                     </div>
                   </div>
                 <div class="col-sm-6">
-                  <div class="card">
-                    <div class="card-body">
+                  <div class="card ">
+                    <div class="card-body d-flex flex-column justify-content-between">
                       <h5 class="card-title text-center">Lista ordini</h5>
                       <img src="https://media.istockphoto.com/id/1256489977/it/vettoriale/controllo-attivit%C3%A0-icona-blu-elenco-di-controllo.jpg?s=612x612&w=0&k=20&c=B0I-Kxb0l8l6pVA3Lqf3hRBK1dtFxIDr09483sxq7xY=" class="card-img-top" alt="...">
 

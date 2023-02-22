bd221d7e73af295e1bde220e1868fe4150519e57
VincenzoZ24 <zitov287@gmail.com>
2023-02-22 12:56:03 +0100

    validations

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -34,7 +34,7 @@
         </div>
 
         <div class="mb-3 form-check">
-        <input value="1" type="checkbox" class="form-check-input" id="visibility" name="visibility">
+        <input value="0" type="checkbox" class="form-check-input" id="visibility" name="visibility">
         <label class="form-check-label" for="visibility">visibilità</label>
         </div>
 

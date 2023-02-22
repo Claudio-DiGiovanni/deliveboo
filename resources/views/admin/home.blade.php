8b40fc826146ef3ff497fece1647e0e39df11052
DiceglieA <angelodiceglie10@gmail.com>
2023-02-22 15:27:35 +0100

    layout homepage

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -19,11 +19,34 @@
             </div> --}}
 
             <h1>Ciao capo, benvenuto nel tuo ristorante</h1>
-            {{-- <ul>
-                @foreach (($user->types) ?? "" as $type)
-                    <li>{{$type->name}}</li>
-                @endforeach
-            </ul> --}}
+           <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                      <div class="card-body">
+                        <h5 class="card-title text-center">Menù piatti</h5>
+                        <img src="https://media-assets.lacucinaitaliana.it/photos/638e66dca4734137a7a3e238/16:9/w_1280,c_limit/spaghetti-spezzati-con-sugo-di-pesce1.jpg" class="card-img-top" alt="...">
+
+                        <div class="row">
+                          <a class="col-10 m-auto btn btn-outline-primary" href="{{ route('admin.dishes.index') }}">Entra</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                <div class="col-sm-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title text-center">Lista ordini</h5>
+                      <img src="https://media.istockphoto.com/id/1256489977/it/vettoriale/controllo-attivit%C3%A0-icona-blu-elenco-di-controllo.jpg?s=612x612&w=0&k=20&c=B0I-Kxb0l8l6pVA3Lqf3hRBK1dtFxIDr09483sxq7xY=" class="card-img-top" alt="...">
+
+                      <div class="row">
+                        <a class="col-10 m-auto btn btn-outline-primary" href="{{ route('admin.orders.index') }}">Entra</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+           </div>
         </div>
     </div>
 </div>

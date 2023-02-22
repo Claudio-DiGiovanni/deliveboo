cd92bc7b589d07237bacaa7716cd1bd5a8d308ee
VincenzoZ24 <zitov287@gmail.com>
2023-02-22 10:33:35 +0100

    search

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,10 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Cerca</button>
+                        </form>
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">

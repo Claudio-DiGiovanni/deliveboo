97a883a64a107d2fa498b6c6e16f78c579b916ef
VincenzoZ24 <zitov287@gmail.com>
2023-02-22 13:09:47 +0100

    validation create

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -7,29 +7,74 @@
 
         <div class="mb-3">
         <label for="name" class="form-label">nome piatto</label>
-        <input type="text" class="form-control" id="name" name="name">
+        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" id="name" name="name">
+                <div class="invalid-feedback">
+                    @error('name')
+                        <ul>
+                            @foreach ($errors->get('name') as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @enderror
+                </div>
 
         </div>
 
         <div class="mb-3">
         <label for="price" class="form-label">prezzo</label>
-        <input type="number" class="form-control" id="price" name="price">
+        <input type="number" class="form-control  @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') }}">
+        <div class="invalid-feedback">
+            @error('price')
+                <ul>
+                    @foreach ($errors->get('price') as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @enderror
+        </div>
         </div>
 
         <div class="mb-3">
             <label for="image" class="form-label">url immagine</label>
-            <input type="url" class="form-control" id="image" name="image">
+            <input type="url" class="form-control  @error('image') is-invalid @enderror" id="image" name="image" value="{{ old('image') }}">
+            <div class="invalid-feedback">
+                @error('image')
+                    <ul>
+                        @foreach ($errors->get('image') as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @enderror
+            </div>
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label">decrizione</label>
-            <input type="text" class="form-control" id="description" name="description">
+            <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" value="{{ old('description') }}"> </textarea>
+            <div class="invalid-feedback">
+                @error('description')
+                    <ul>
+                        @foreach ($errors->get('description') as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @enderror
+            </div>
 
         </div>
 
         <div class="mb-3">
             <label for="slug" class="form-label">slug</label>
-            <input type="text" class="form-control" id="slug" name="slug">
+            <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" value="{{ old('slug') }}">
+            <div class="invalid-feedback">
+                @error('slug')
+                    <ul>
+                        @foreach ($errors->get('slug') as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @enderror
+            </div>
 
         </div>
 

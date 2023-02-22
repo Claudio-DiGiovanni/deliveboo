f0b24eb28dfe712b3cfc019be0318d73b81fde25
doctorpack <enricopachera@gmail.com>
2023-02-22 12:00:47 +0100

    style-prova

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Ordini:</h1>
+
+<h1 class="justify-content-center d-flex">Lista ordini</h1>
+
+<div class="container d-flex justify-content-center flex-column align-items-center ">
+
 @foreach ($orders as $order)
-<div class="row">
-    <ul class="col-5">
-        <a href="{{ route('admin.orders.show', ['order' => $order]) }}" class="btn btn-outline-info">Info</a>
-        <li>
+<div class="">
+    <ul class="m-3 list-style-none; ">
+        <li class="">
             {{$order->order_number}}
         </li>
         <li>
             {{$order->created_at}}
         </li>
+        <a href="{{ route('admin.orders.show', ['order' => $order]) }}" class="btn btn-outline-info">Info</a>
     </ul>
 </div>
 @endforeach

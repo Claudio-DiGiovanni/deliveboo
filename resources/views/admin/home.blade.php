16131e21d7f88b067aab7fb12c88744ade7e4c3b
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-02-23 09:48:43 +0100

    auth

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+@auth
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -36,4 +37,6 @@
         </div>
     </div>
 </div>
+@endauth
+
 @endsection

87a73b6621d8d3a24939ed477b1541f0c79d8039
AdrianBottini <adrianboh@gmail.com>
2023-02-23 12:14:14 +0100

    style_card

--- resources/js/pages/PageHome.vue
+++ resources/js/pages/PageHome.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="container">
-        <ul>
-            <li v-for="user in users" :key="user.id"><router-link :to="{name: 'dishes'}" class="nav-link">{{ user.name }}</router-link></li>
-        </ul>
+        <div class="container">
+        <div class="card" v-for="user in users" :key="user.id" style="width: 18rem;">
+            <img :src="user.image_logo" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 class="card-text"><router-link :to="{ name: 'dishes' }">{{ user.name }}</router-link></h2>
+            </div>
+        </div>
+    </div>
     </div>
 </template>
 

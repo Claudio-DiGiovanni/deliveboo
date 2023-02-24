4f090d6dbfc15484d07587e60daaf2cc21c891c3
AdrianBottini <adrianboh@gmail.com>
2023-02-24 10:35:17 +0100

    card_finite

--- resources/js/pages/PageHome.vue
+++ resources/js/pages/PageHome.vue
@@ -1,8 +1,17 @@
 <template>
-    <div class="container">
-        <ul>
-            <li v-for="user in users" :key="user.id"><router-link :to="{name: 'dishes', params: { id: user.id }}" class="nav-link">{{ user.name }}</router-link></li>
-        </ul>
+   <div class="container ">
+        <div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-2" >
+            <router-link :to="{name: 'dishes', params: { id: user.id }}" class="p-3 w-75 m-auto" v-for="user in users" :key="user.id">
+            <div class="card card-ristoranti bg-opacity-25 col ">
+                <div class="w-75 h-75 m-auto">
+                    <img :src="user.image_logo" class="card-img-top p-2 h-100 w-100" alt="...">
+                </div>
+                <div class="card-body text-center">
+                    <h2 class="card-text text-dark m-3">{{ user.name }}</h2>
+                </div>
+            </div>
+        </router-link>
+        </div>
     </div>
 </template>
 
@@ -26,6 +35,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    a {
+        text-decoration: none;
+    }
+    .card-ristoranti {
+        height: 20rem;
+        background-color: rgb(154, 234, 197);
+    }
 </style>

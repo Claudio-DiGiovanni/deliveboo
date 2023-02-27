22073c16a9a58d0d3feb84d52d165ae7dbc09dc4
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-02-27 11:46:20 +0100

    .

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Braintree\Provider\BraintreeServiceProvider::class,
+        //Braintree\Provider\BraintreeServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Braintree' => Braintree\Facade\Braintree::class,
+        //'Braintree' => Braintree\Facade\Braintree::class,
 
     ],
 

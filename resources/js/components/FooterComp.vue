a4ce4650a1ac50bac3349c8d1b3353544d5d8e98
DiceglieA <angelodiceglie10@gmail.com>
2023-03-02 17:37:57 +0100

    modifiche foot

--- resources/js/components/FooterComp.vue
+++ resources/js/components/FooterComp.vue
@@ -1,25 +1,27 @@
 <template>
-  <div>
-    <div class="container row row-cols-4">
-        <ul>
-            <li>Scopri Deliveroo</li>
+  <div class="bg">
+    <div class="container">
+        <div class=" row row-cols-3">
+        <ul class="col">
+            <li><h5>Scopri Deliveroo</h5></li>
             <li><a href="#!">Chi siamo</a></li>
             <li><a href="#!">Ristoranti</a></li>
             <li><a href="#!">altro</a></li>
             <li><a href="#!">Lavora con noi</a></li>
             <li><a href="#!">Diventa nostro partner</a></li>
         </ul>
-        <ul>
-            <li>Note</li>
+        <ul class="col">
+            <li><h5>Note</h5></li>
             <li><a href="#!">Termini & Condizioni</a></li>
             <li><a href="#!">Privacy</a></li>
             <li><a href="#!">Cookies</a></li>
         </ul>
-        <ul>
-            <li>Aiuto</li>
+        <ul class="col">
+            <li><h5>Aiuto</h5></li>
             <li><a href="#!">Contatti</a></li>
             <li><a href="#!">FAQ</a></li>
         </ul>
+        </div>
     </div>
   </div>
 </template>
@@ -31,5 +33,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.bg {
+    background-color: #2e3333;
+}
+ul {
+    color: white;
+    text-decoration: none;
+    list-style-type: none;
+    a {
+        color: white;
+    }
+}
 </style>

d1d5fd018983c552f4daf5ab2a5ea525c804b695
DiceglieA <angelodiceglie10@gmail.com>
2023-03-02 17:05:13 +0100

    base footer + img logo

---
+++ resources/js/components/FooterComp.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="container row row-cols-4">
+        <ul>
+            <li>Scopri Deliveroo</li>
+            <li><a href="#!">Chi siamo</a></li>
+            <li><a href="#!">Ristoranti</a></li>
+            <li><a href="#!">altro</a></li>
+            <li><a href="#!">Lavora con noi</a></li>
+            <li><a href="#!">Diventa nostro partner</a></li>
+        </ul>
+        <ul>
+            <li>Note</li>
+            <li><a href="#!">Termini & Condizioni</a></li>
+            <li><a href="#!">Privacy</a></li>
+            <li><a href="#!">Cookies</a></li>
+        </ul>
+        <ul>
+            <li>Aiuto</li>
+            <li><a href="#!">Contatti</a></li>
+            <li><a href="#!">FAQ</a></li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

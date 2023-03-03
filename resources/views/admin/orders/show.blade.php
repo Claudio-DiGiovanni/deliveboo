650a39c6c351c20acdf49df20eaa274820840cb0
VincenzoZ24 <zitov287@gmail.com>
2023-03-03 12:48:57 +0100

    count

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -2,59 +2,49 @@
 @section('content')
     <div>
         <div class="background_img">
-            <img class="img_background" src="https://media.discordapp.net/attachments/1031881470786154557/1079778610757521480/deliveboo-upscaling.png" alt="">
-            <div class="container  container_home d-flex justify-content-center align-items-center flex-column h-100 container_show_order">
+            <img class="img_background"
+                src="https://media.discordapp.net/attachments/1031881470786154557/1079778610757521480/deliveboo-upscaling.png"
+                alt="">
+            <div
+                class="container  container_home d-flex justify-content-center align-items-center flex-column h-100 container_show_order">
                 <div class="container_order">
-                    <table class="table table-striped table_orders table-bordered">
-                        <thead>
-                            <tr class="table-success">
-                                <th>Numero ordine</th>
-                                <th>Prezzo totale</th>
-                                <th>Via</th>
-                                <th>Nome</th>
-                                <th>Email</th>
-                                <th>Piatti</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td> {{ $order->id }}</td>
-                                <td> {{ $total_cost / 100 }} €</td>
-                                <td> {{ $order->address }}</td>
-                                <td> {{ $order->customer_name }}</td>
-                                <td> {{ $order->email }}</td>
+                    <div class="content_show_orders">
+                        <div>
 
-                                <td class="td_show_orders white">
-                                    <ul class="p-0">
-                                        @foreach ($dishes as $dish)
-                                            <li> {{ $dish->name }}</li>
-                                        @endforeach
-                                    </ul>
+                            <div> <span class="grassetto">Numero ordine :</span> {{ $order->id }}</div>
+                            <div> <span class="grassetto">Prezzo totale :</span> {{ $total_cost / 100 }} €</div>
+                            <div> <span class="grassetto">Via :</span> {{ $order->address }}</div>
+                            <div> <span class="grassetto">Nome :</span> {{ $order->customer_name }}</div>
+                            <div> <span class="grassetto">Email :</span> {{ $order->email }}</div>
+                            <div><span class="grassetto">Piatti :</span>
+                                <ul>
+                                    @foreach ($dishes as $dish)
+                                        <li> {{ $dish->name }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
 
-                                </td>
-                                <td class="white">
-                                    <button id="delete" class="btn btn-outline-danger" onclick="showPopup(event)"
-                                        data-id="{{ $order->id }}">Elimina</button>
-                                    <div class="background">
-                                        <div class="popup">
-                                            <h5 class="w-100 text-center mb-3">Sei sicuro di volerlo eliminare?</h5>
-                                            <form action="{{ route('admin.orders.destroy', ['order' => $order]) }}"
-                                                method="post">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button class="btn btn-danger">Elimina</button>
-                                            </form>
-                                            <button class="btn btn-secondary" id="retry"
-                                                onclick="hidePopup()">Annulla</button>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
 
-                        </tbody>
+                        </div>
 
-                    </table>
+                        <div class="white">
+                            <button id="delete" class="btn btn-outline-danger" onclick="showPopup(event)"
+                                data-id="{{ $order->id }}">Elimina</button>
+                            <div class="background">
+                                <div class="popup">
+                                    <h5 class="w-100 text-center mb-3">Sei sicuro di volerlo eliminare?</h5>
+                                    <form action="{{ route('admin.orders.destroy', ['order' => $order]) }}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button class="btn btn-danger">Elimina</button>
+                                    </form>
+                                    <button class="btn btn-secondary" id="retry" onclick="hidePopup()">Annulla</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
 
                 </div>
 
@@ -64,4 +54,26 @@
         </div>
 
     </div>
+    <ol class="list-group list-group-numbered">
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            @foreach ($dishes as $dish)
+                <div class="ms-2 me-auto">
+
+                    {{ $dish->name }}
+                   
+                      
+                      
+                            
+                           
+                    
+                   
+                        
+                </div>
+                <span class="badge bg-primary rounded-pill">
+                    {{$count}}
+                   </span>
+            @endforeach
+            
+        </li>
+    </ol>
 @endsection
